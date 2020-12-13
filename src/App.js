175fdc0da3ac259ae2c1994b315444a25828a3e8
ShinYS88 <sinyusu129@gmail.com>
2020-12-13 23:18:00 +0900

    'commit.'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 // BrowserRouter : github pages 등록시 정확히 설정이 어렵다. => HashRouter 사용을 추천
 
